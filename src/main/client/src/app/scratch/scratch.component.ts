6411d95afcb696b07f4c9888418978c69059fea4
reffx <ripsli@hotmail.com>
2019-04-29 08:34:20 +0200

    FRONTEND, Task 1: New Products can be created

--- src/main/client/src/app/scratch/scratch.component.ts
+++ src/main/client/src/app/scratch/scratch.component.ts
@@ -9,17 +9,36 @@ import {Product} from "./product";
   styleUrls: ['./scratch.component.css']
 })
 export class ScratchComponent implements OnInit {
-
+  productName: "";
+  userName: "";
+  purchaseDate: "";
+  price: null;
 
   users: User[];
   products: Product[];
-
 
   loadedAt: string;
 
   constructor( private httpClient:HttpClient) { }
 
   ngOnInit() {
+    this.getProducts();
+  }
+
+  onProductNameKeyUp(event: any) {
+    this.productName = event.target.value;
+  }
+
+  onUserNameKeyUp(event: any) {
+    this.userName = event.target.value;
+  }
+
+  onPurchaseDateKeyUp(event: any) {
+    this.purchaseDate = event.target.value;
+  }
+
+  onPriceKeyUp(event: any) {
+    this.price = event.target.value;
   }
 
   getProducts() {
@@ -29,6 +48,31 @@ export class ScratchComponent implements OnInit {
         this.products = resp;
       });
     this.loadedAt = new Date().toLocaleTimeString();
+  }
+
+  postProduct() {
+    this.httpClient.post("api/products", {
+      productName: this.productName,
+      userName: this.userName,
+      purchaseDate: this.purchaseDate,
+      price: this.price
+    })
+      .subscribe((resp) => {
+        console.log(resp);
+        this.getProducts();
+      })
+  }
+
+  updateProduct(productId: number) {
+    this.httpClient.put("api/products/" + productId, {
+      productName: this.productName,
+      userName: this.userName,
+      purchaseDate: this.purchaseDate,
+      price: this.price
+    }).subscribe((resp) => {
+      console.log(productId);
+      this.getProducts();
+    });
   }
 
   onLoadUsersButtonClick() {
